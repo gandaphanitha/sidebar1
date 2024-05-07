7f2f2ad1b19e66752d44607d7df47ff963148fb7
gandaphanitha <161686624+gandaphanitha@users.noreply.github.com>
2024-05-07 20:22:43 +0530

    Add files via upload

---
+++ sidebar.js
@@ -0,0 +1,8 @@
+const openNav=()=>{
+    document.querySelector(".sidebar").style.width="250px"
+    document.querySelector(".sidebar").style.display="block"
+}
+const closeNav=()=>{
+    document.querySelector(".sidebar").style.width="0px"
+    document.querySelector(".sidebar").style.display="none"
+}
